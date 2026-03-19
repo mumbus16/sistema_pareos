f25d1e297cd703b598fe07ec8821d3e4b4a6fafc
mumbus16 <139721236+mumbus16@users.noreply.github.com>
2026-03-19 10:40:41 -0600

    script.js

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ document.addEventListener("DOMContentLoaded", () => {
             const pass = document.getElementById("password").value;
 
             try {
-                const response = await fetch('http://localhost:3000/api/login', {
+                const response = await fetch('https://sistema-pareos.onrender.com//api/login', {
                     method: 'POST',
                     headers: { 'Content-Type': 'application/json' },
                     body: JSON.stringify({ usuario: user, password: pass })
