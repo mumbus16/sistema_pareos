2ec6e7efe20bd4ab29a0b933df783f8e2b894d75
mumbus16 <139721236+mumbus16@users.noreply.github.com>
2026-03-19 11:14:31 -0600

    Update server.js

--- server.js
+++ server.js
@@ -10,10 +10,10 @@ app.use(cors());
 // --- CONFIGURACIÓN DE CONEXIÓN (POOL DE PROMESAS) ---
 const db = mysql.createPool({
     host: process.env.MYSQLHOST,
-user: process.env.MYSQLUSER,
-password: process.env.MYSQLPASSWORD,
-database: process.env.MYSQLDATABASE,
-port: process.env.MYSQLPORT,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
+    port: process.env.MYSQLPORT,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
